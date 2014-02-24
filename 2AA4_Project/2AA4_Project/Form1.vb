f278a87c9601f2defb61c32021837398433a26bc
challett <connor.hallett@gmail.com>
2014-02-24 13:04:20 -0500

    First Code

--- 2AA4_Project/2AA4_Project/Form1.vb
+++ 2AA4_Project/2AA4_Project/Form1.vb
@@ -1,3 +1,19 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim size As Integer
+        Dim trackarray(0 To 63) As Label
+
+        size = 63
+
+        For i = 0 To size
+            trackarray(i) = New Label
+            trackarray(i).Top = 0
+            trackarray(i).Text = "something"
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        End
+    End Sub
 End Class
